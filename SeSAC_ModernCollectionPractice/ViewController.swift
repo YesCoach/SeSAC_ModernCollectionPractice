aac89c794cd8f40bbf5bcdebc8d5d3af43d82610
YesCoach <sbsii1020@gmail.com>
2023-09-15 14:44:01 +0900

    Initial Commit

---
+++ SeSAC_ModernCollectionPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_ModernCollectionPractice
+//
+//  Created by 박태현 on 2023/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
